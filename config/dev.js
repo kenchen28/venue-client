f6f4f5be3389554a0016ff9a444f8d4e9cf733db
Ken Chen <ken.chen@tabcorp.com.au>
2025-08-19 19:23:44 +1000

    Updates

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+window.config = {
+  invenueHost: 'https://uat01.beta.tab.com.au',
+  //deviceId: 'test-device-001', // Temporary until navigator.managed works
+  checkInterval: 10000,
+  autoFullscreen: false,
+  autoFullscreenDelay: 5000,
+  environment: 'development'
+};
